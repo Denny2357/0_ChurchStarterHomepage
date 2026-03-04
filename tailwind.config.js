ee6a7e8d99b065a09426c0bb408876694fdb7e1d
이종현 <dennylee@ijonghyeons-MacBook-Pro.local>
2026-03-04 15:31:46 +0900

    style: logo decoration added

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // src 폴더 안의 모든 html, js 파일을 감시!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
